f9ae5a314aa3af45646fd674396631a5457a204a
Evan Greif <egreif1@gmail.com>
2013-11-14 23:04:48 +0000

    updated code

--- text_helpers.py
+++ text_helpers.py
@@ -2,8 +2,15 @@
 # This is a module of python functions
 # that will help you deal with text data
 
-import string
 
+# dependence
+import string
+import functional
+
+
+# global variables
+trivial_loaded = False
+trivial = {}
 
 
 # This method tests if a string is
@@ -29,7 +36,7 @@ def is_word(word):
 # a word and return an ascii string
 # Use case: you think the word is ascii
 # and want ending punctuation stripped from it
-def clean_ascii(word):
+def strip_punc(word):
 
 
 	pass
@@ -41,14 +48,62 @@ def clean_ascii(word):
 # don't support that many emoticons, but
 # ultimately we will
 def is_emoticon(word):
-	pass
+
+	# check for smilies
+	if (word == ":)"):
+		return True
+	elif (word == ":/"):
+		return True
+	elif (word == ":("):
+		return True
+		
+	
+
+# Adds a word to the trivial dictionary
+def add_trivial(word):
+
+	# declare that we will change global variable
+	global trivial;
+
+	# strip whitespace
+	word = word.strip()
+
+	# add entry to dictionary
+	trivial[word] = 1
+
+
+
+# Loads the set of trivial words in the
+# containing directory
+def load_trivial():
+
+	# just apply the function across the trivial file
+	functional.apply(add_trivial,"trivial.txt")
 	
 
 
-# This is a function to clean
-# an emoticon
-def clean_emoticon(word):
-	pass
+
+
+# This is a functio to determine whether a
+# given word is trivial, or should be
+# considered significant
+def is_trivial(word):
+
+	# switch to lowercase
+	word = word.lower()
+
+	# check that we've loaded the trivial list
+	if (not(trivial_loaded)):
+		load_trivial()
+
+	# now check if the word is trivial
+	try:
+		trivial[word]
+	except KeyError:
+		return False
+	return True
+	
+
 
 
 
