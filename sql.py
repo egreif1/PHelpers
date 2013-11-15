d8b136f305aca3f7bc9b49f5cad817f3fe205ec8
Evan Greif <egreif1@gmail.com>
2013-11-15 10:58:43 +0000

    added functional methods for sql

--- sql.py
+++ sql.py
@@ -13,3 +13,18 @@ def connect(database):
 
 
 
+# method for running a function
+# on every result from a query
+# takes function to apply, the sql query
+# and the cursor for the database
+def apply_search(fun,sql,cur):
+
+	# execute the query
+	cur.execute(sql)
+
+	# now go through the result and execute function
+	data = cur.fetchall()
+	for row in data:
+		fun(row)
+
+
